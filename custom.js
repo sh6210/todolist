51fc51c01d7848b961f52d2213321a5c84a64dae
Md. Sohag Hossain <shcseilacs@gmail.com>
2018-12-21 10:01:35 +0600

    modified js according to needs

--- custom.js
+++ custom.js
@@ -2,7 +2,6 @@ var active = null;
 
 $('#myModal').on('hidden.bs.modal', function (e) {
     active = null;
-    console.log(getActive());
 });
 
 function closeModal() {
@@ -35,6 +34,12 @@ function saveTodo() {
 
     let newLi = document.createElement('li');
 
+    setAttributes(newLi, {
+        "id": getRandomId(),
+        "draggable": "true",
+        "ondragstart": "drag(event)"
+    });
+
     let dummyNmae = 'You';
     let dummyTimer = 'Now';
 
@@ -56,4 +61,31 @@ function saveTodo() {
     closeModal();
 
     $("#message-text").val('');
+}
+
+function drag(e) {
+    e.dataTransfer.setData("text", e.target.id);
+}
+
+function allowDrop(e) {
+    e.preventDefault();
+}
+
+function drop(e) {
+    e.preventDefault();
+    var data = e.dataTransfer.getData('text');
+    e.target.closest('ul').insertBefore(document.getElementById(data), e.target.closest('li'));
+}
+
+function getRandomId() {
+    let today = new Date();
+    let dateTime = today.getDate() + today.getTime();
+    let randomeNumber = Math.ceil(Math.random() * 98765);
+    return dateTime * randomeNumber;
+}
+
+function setAttributes(el, attrs) {
+    for (var key in attrs) {
+        el.setAttribute(key, attrs[key]);
+    }
 }
